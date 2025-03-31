4cd254ec412c7f7880a1a27dd6a0bf0d271d2f31
Timur <156953282+TimurBased@users.noreply.github.com>
2025-03-31 14:41:30 +0300

    fix: react router & netlify

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	resolve: {
-		alias: { '@': path.resolve(__dirname, './src') },
-	},
-	server: {
-		port: 3000,
-	},
+  plugins: [react(), netlifyPlugin()],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  server: {
+    port: 3000,
+  },
 })
